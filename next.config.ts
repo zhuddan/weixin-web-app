ece3aade9c22fd425586bc567f272329d2190537
zhuddan <yobaci@163.com>
2024-11-13 17:20:53 +0800

    git

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 // import UnpluginIcons from 'unplugin-icons/webpack'
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
   output: 'standalone'
 };
 

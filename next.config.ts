c918475395e32c8977887d19f65b45e1b475274a
zhuddan <yobaci@163.com>
2024-11-13 16:24:28 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,8 @@ import type { NextConfig } from "next";
 // import UnpluginIcons from 'unplugin-icons/webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // webpack(config) {
-  //   config.plugins.push(
-  //     UnpluginIcons({
-  //       compiler: 'jsx',
-  //       jsx: 'react'
-  //     })
-  //   )
-  //   return config
-  // }
-  trailingSlash: true
+  trailingSlash: true,
+  output: 'standalone'
 };
 
 export default nextConfig;
